9eded2f1e9acffd8a58873424c2439b4f5d10c07
Lachlan Deer <lachlan.deer@gmail.com>
2020-02-06 03:38:46 -0600

    modify shell script to handle bib management, #1

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,3 +1,6 @@
 pandoc --template=asa_template.tex \
     my_paper.md \
+    --filter pandoc-citeproc \
+    --csl chicago-fullnote-bibliography.csl \
+    --bibliography bibliography.bib \
     --pdf-engine=pdflatex -o out/example_paper.pdf
